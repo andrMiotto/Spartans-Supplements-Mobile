9a8a0682d1562cb4ec001af5eca3f643bd294e75
JOSÉ AZARÍAS PÉREZ TORRES <jose_a_torres@estudante.sesisenai.org.br>
2026-03-25 15:10:54 -0300

    feat: implementa funções de chamada, da tela login e da tela register

--- app/src/main/java/com/example/spartans_supplements_sobile/MainActivity.kt
+++ app/src/main/java/com/example/spartans_supplements_sobile/MainActivity.kt
@@ -7,7 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.spartans_supplements_sobile.screens.DetailScreen
+import com.example.login.LoginScreenFuntion
+import com.example.spartans_supplements_sobile.screens.*
 import com.example.spartans_supplements_sobile.screens.StoreHomeScreen
 import com.example.spartans_supplements_sobile.ui.theme.SpartansSupplementsSobileTheme
 
@@ -20,10 +21,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DetailScreen();
+                    setContent {
+                        RegisterScreenFuntion();
+                    }
                 }
             }
         }
     }
+
+
 }
 
