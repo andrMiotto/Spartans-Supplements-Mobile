29ee02e889a1afc64dca901e3192db36394fc009
André Luis <andre_lm_pereira@estudante.sesisenai.org.br>
2026-03-26 15:29:21 -0300

    Revert "Feature/api communication"

--- app/src/main/java/com/example/spartans_supplements_sobile/MainActivity.kt
+++ app/src/main/java/com/example/spartans_supplements_sobile/MainActivity.kt
@@ -1,4 +1,4 @@
-    package com.example.spartans_supplements_sobile
+package com.example.spartans_supplements_sobile
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,47 +6,41 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.*
-import com.example.login.LoginScreenFuntion
-import com.example.spartans_supplements_sobile.screens.CartScreen
-import com.example.spartans_supplements_sobile.screens.RegisterScreenFuntion
-import com.example.spartans_supplements_sobile.screens.StoreHomeScreen
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.spartans_supplements_sobile.ui.theme.SpartansSupplementsSobileTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             SpartansSupplementsSobileTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    val navController = rememberNavController()
-
-                    NavHost(navController = navController, startDestination = "login") {
-
-                        composable("login") {
-                            LoginScreenFuntion(navController)
-                        }
-
-                        composable("register") {
-                            RegisterScreenFuntion(navController)
-                        }
-
-                        composable("home") {
-                            StoreHomeScreen(navController)
-                        }
-
-                        composable("cart") {
-                            CartScreen(navController)
-                        }
-                    }
+                    Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SpartansSupplementsSobileTheme {
+        Greeting("Android")
     }
 }
